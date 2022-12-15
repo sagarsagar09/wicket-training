f514e901f1ce176b9c23f7f5cb0ac2964bdb457b
gauri <user@DESKTOP-SFMV7A8>
2022-12-15 12:34:19 +0530

    declared datatypes and generated getters,setters,hashcode,equals and toString

--- wicket-training/src/main/java/com/gel/GauriTest.java
+++ wicket-training/src/main/java/com/gel/GauriTest.java
@@ -17,7 +17,6 @@ public class GauriTest {
 		
 	}
 
-
 	@Override
 	public String toString() {
 		return "GauriTest [age=" + age + ", name=" + name + ", False=" + False + ", f=" + f + ", s=" + s + ", a=" + a
