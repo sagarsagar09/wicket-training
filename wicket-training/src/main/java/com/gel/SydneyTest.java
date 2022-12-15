cc5236345e4fba0fba3941dbbac599b76b19e5b5
Sydney <sydneyaraujo.54321@gmail.com>
2022-12-15 11:52:11 +0530

    File update: Added getters and setters methods and toString()

--- wicket-training/src/main/java/com/gel/SydneyTest.java
+++ wicket-training/src/main/java/com/gel/SydneyTest.java
@@ -5,6 +5,46 @@ public class SydneyTest {
 	private int age;
 	private String firstName;
 	private String lastName;
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+
+	@Override
+	public String toString() {
+		return "SydneyTest [name=" + name + ", age=" + age + ", firstName=" + firstName + ", lastName=" + lastName
+				+ "]";
+	}
 	
 	public static void main(String args[]) {
 		System.out.println("Hello World");
