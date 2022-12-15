a8e33598cf481cf23bb93c75b1766a5c6c444caf
Yogesh <yogesh.a.pawar155@gmail.com>
2022-12-15 12:54:00 +0530

    Merge branch 'main' of https://github.com/shivain22/wicket-training into main

--- wicket-training/src/main/java/com/gel/YogeshTest.java
+++ wicket-training/src/main/java/com/gel/YogeshTest.java
@@ -1,5 +1,97 @@
 package com.gel;
-
 public class YogeshTest {
+	private int myNum = 5;
+	private float myFloatNum = 5.99f;
+	private char myLetter = 'D';
+	private boolean myBool = true;
+	private String myText = "Hello";
+	
+	public static void main(String args[]) {
+		System.out.println("Hello World");
+	}
 
+	@Override
+	public String toString() {
+		return "YogeshTest [myNum=" + myNum + ", myFloatNum=" + myFloatNum + ", myLetter=" + myLetter + ", myBool="
+				+ myBool + ", myText=" + myText + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (myBool ? 1231 : 1237);
+		result = prime * result + Float.floatToIntBits(myFloatNum);
+		result = prime * result + myLetter;
+		result = prime * result + myNum;
+		result = prime * result + ((myText == null) ? 0 : myText.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		YogeshTest other = (YogeshTest) obj;
+		if (myBool != other.myBool)
+			return false;
+		if (Float.floatToIntBits(myFloatNum) != Float.floatToIntBits(other.myFloatNum))
+			return false;
+		if (myLetter != other.myLetter)
+			return false;
+		if (myNum != other.myNum)
+			return false;
+		if (myText == null) {
+			if (other.myText != null)
+				return false;
+		} else if (!myText.equals(other.myText))
+			return false;
+		return true;
+	}
+
+	public int getMyNum() {
+		return myNum;
+	}
+
+	public void setMyNum(int myNum) {
+		this.myNum = myNum;
+	}
+
+	public float getMyFloatNum() {
+		return myFloatNum;
+	}
+
+	public void setMyFloatNum(float myFloatNum) {
+		this.myFloatNum = myFloatNum;
+	}
+
+	public char getMyLetter() {
+		return myLetter;
+	}
+
+	public void setMyLetter(char myLetter) {
+		this.myLetter = myLetter;
+	}
+
+	public boolean isMyBool() {
+		return myBool;
+	}
+
+	public void setMyBool(boolean myBool) {
+		this.myBool = myBool;
+	}
+
+	public String getMyText() {
+		return myText;
+	}
+
+	public void setMyText(String myText) {
+		this.myText = myText;
+	}
+	
+	
 }
